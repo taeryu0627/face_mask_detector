79f89e4b66158688537d1202f344f9ab5acaa4b0
taeryu <taeryu0627@naver.com>
2021-04-12 21:03:12 +0900

    pyqt5 창띄우기
    구현

--- main.py
+++ main.py
@@ -1,2 +1,22 @@
-a = 10
-print(dir(a))
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow
+
+
+class MyApp(QMainWindow):
+
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+        self.statusBar().showMessage('Ready')
+
+        self.setWindowTitle('Statusbar')
+        self.setGeometry(300, 300, 300, 200)
+        self.show()
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = MyApp()
+    sys.exit(app.exec_())
