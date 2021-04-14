fd567774bee3f65c30beb2b5bd14cb4e1b67fc55
taeryu <taeryu0627@naver.com>
2021-04-14 21:56:18 +0900

    난수 생성
    고정 layout 필요

--- main.py
+++ main.py
@@ -1,98 +1,35 @@
-from PyQt5.QtGui import QIcon, QFont
-from PyQt5.QtCore import QDir, Qt, QUrl, QSize
-from PyQt5.QtMultimedia import QMediaContent, QMediaPlayer
-from PyQt5.QtMultimediaWidgets import QVideoWidget
-from PyQt5.QtWidgets import (QApplication, QFileDialog, QHBoxLayout, QLabel,
-                             QPushButton, QSizePolicy, QSlider, QStyle, QVBoxLayout, QWidget, QStatusBar)
+## Ex 6-1. QInputDialog.
+
+import sys
+from PyQt5.QtWidgets import (QApplication, QWidget, QPushButton, QLineEdit, QInputDialog)
 
 
-class VideoPlayer(QWidget):
-    def __init__(self, parent=None):
-        super(VideoPlayer, self).__init__(parent)
-        self.mediaPlayer = QMediaPlayer(None, QMediaPlayer.VideoSurface)
-        btnSize = QSize(16, 16)
-        videoWidget = QVideoWidget()
-        openButton = QPushButton("Open Video")
-        openButton.setToolTip("Open Video File")
-        openButton.setStatusTip("Open Video File")
-        openButton.setFixedHeight(24)
-        openButton.setIconSize(btnSize)
-        openButton.setFont(QFont("Noto Sans", 8))
-        openButton.setIcon(QIcon.fromTheme("document-open", QIcon("D:/_Qt/img/open.png")))
-        openButton.clicked.connect(self.abrir)
-        self.playButton = QPushButton()
-        self.playButton.setEnabled(False)
-        self.playButton.setFixedHeight(24)
-        self.playButton.setIconSize(btnSize)
-        self.playButton.setIcon(self.style().standardIcon(QStyle.SP_MediaPlay))
-        self.playButton.clicked.connect(self.play)
-        self.positionSlider = QSlider(Qt.Horizontal)
-        self.positionSlider.setRange(0, 0)
-        self.positionSlider.sliderMoved.connect(self.setPosition)
-        self.statusBar = QStatusBar()
-        self.statusBar.setFont(QFont("Noto Sans", 7))
-        self.statusBar.setFixedHeight(14)
-        controlLayout = QHBoxLayout()
-        controlLayout.setContentsMargins(0, 0, 0, 0)
-        controlLayout.addWidget(openButton)
-        controlLayout.addWidget(self.playButton)
-        controlLayout.addWidget(self.positionSlider)
-        layout = QVBoxLayout()
-        layout.addWidget(videoWidget)
-        layout.addLayout(controlLayout)
-        layout.addWidget(self.statusBar)
-        self.setLayout(layout)
-        self.mediaPlayer.setVideoOutput(videoWidget)
-        self.mediaPlayer.stateChanged.connect(self.mediaStateChanged)
-        self.mediaPlayer.positionChanged.connect(self.positionChanged)
-        self.mediaPlayer.durationChanged.connect(self.durationChanged)
-        self.mediaPlayer.error.connect(self.handleError)
-        self.statusBar.showMessage("Ready") ## 준비창
+class MyApp(QWidget):
 
-    def abrir(self):
-        fileName, _ = QFileDialog.getOpenFileName(self, "Selecciona los mediose",
-                                                  ".", "Video Files (*.mp4 *.flv *.ts *.mts *.avi)")
-        if fileName != '':
-            self.mediaPlayer.setMedia(
-                QMediaContent(QUrl.fromLocalFile(fileName)))
-            self.playButton.setEnabled(True)
-            self.statusBar.showMessage(fileName)
-            self.play()
+    def __init__(self):
+        super().__init__()
+        self.initUI()
 
-    def play(self):
-        if self.mediaPlayer.state() == QMediaPlayer.PlayingState:
-            self.mediaPlayer.pause()
-        else:
-            self.mediaPlayer.play()
+    def initUI(self):
+        self.btn = QPushButton('Dialog', self)
+        self.btn.move(30, 30)
+        self.btn.clicked.connect(self.showDialog)
 
-    def mediaStateChanged(self, state):
-        if self.mediaPlayer.state() == QMediaPlayer.PlayingState:
-            self.playButton.setIcon(
-                self.style().standardIcon(QStyle.SP_MediaPause))
-        else:
-            self.playButton.setIcon(
-                self.style().standardIcon(QStyle.SP_MediaPlay))
+        self.le = QLineEdit(self)
+        self.le.move(120, 35)
 
-    def positionChanged(self, position):
-        self.positionSlider.setValue(position)
+        self.setWindowTitle('Input dialog')
+        self.setGeometry(300, 300, 300, 200)
+        self.show()
 
-    def durationChanged(self, duration):
-        self.positionSlider.setRange(0, duration)
+    def showDialog(self):
+        text, ok = QInputDialog.getText(self, 'Input Dialog', 'Enter your name:')
 
-    def setPosition(self, position):
-        self.mediaPlayer.setPosition(position)
-
-    def handleError(self):
-        self.playButton.setEnabled(False)
-        self.statusBar.showMessage("Error: " + self.mediaPlayer.errorString())
+        if ok:
+            self.le.setText(str(text))
 
 
 if __name__ == '__main__':
-    import sys
-
     app = QApplication(sys.argv)
-    player = VideoPlayer()
-    player.setWindowTitle("Player")
-    player.resize(600, 400)
-    player.show()
+    ex = MyApp()
     sys.exit(app.exec_())
