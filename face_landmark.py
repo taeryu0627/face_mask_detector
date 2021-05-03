f3787033199998c7bc57515ffd67748fe5164d20
taeryu <taeryu0627@naver.com>
2021-05-03 21:57:21 +0900

    landmarks
    이미지의 랜드마크 설정 및 이미지 다운로드
    턱의 점을 기준으로 마스크를 씌움

---
+++ face_landmark.py
@@ -0,0 +1,53 @@
+# 얼굴 랜드마크 추출
+
+import face_recognition
+from PIL import Image, ImageDraw
+
+face_image_path = 'data/without_mask/104.jpg'
+mask_image_path = 'data/mask.png'
+
+face_image_np = face_recognition.load_image_file(face_image_path)
+face_locations = face_recognition.face_locations(face_image_np)
+face_landmarks = face_recognition.face_landmarks(face_image_np, face_locations)
+
+face_landmark_image = Image.fromarray(face_image_np)
+draw = ImageDraw.Draw(face_landmark_image)
+
+maxX = 0
+maxY = 0
+minX = 10000
+minY = 10000
+
+for face_landmark in face_landmarks:
+   for feature_name, points in face_landmark.items():
+       print(feature_name,points)
+       if feature_name == "chin":
+           for x, y in points:
+               if minX > x:
+                   minX = x
+               if minY > y:
+                   minY = y
+               if maxX < x:
+                   maxX = x
+               if maxY < y:
+                   maxY = y
+
+print(maxX,minX,maxY,minY)
+
+for face_location in face_locations:
+    top = face_location[0]
+    right = face_location[1]
+    bottom = face_location[2]
+    left = face_location[3]
+    draw.rectangle(((left, top), (right, bottom)), outline=(255, 255, 0), width=4)
+
+mask_image_np = Image.fromarray(face_image_np)
+draw = ImageDraw.Draw(mask_image_np)
+
+mask_image = Image.open(mask_image_path)
+mask_image = mask_image.resize((int(maxX)-int(minX),int(maxY)-int(minY)))
+
+face_landmark_image.paste(mask_image,(int(minX), int(minY)),mask_image)
+
+
+face_landmark_image.show()
--- image_downloader.py
+++ image_downloader.py
@@ -1,19 +1,8 @@
+# 사람 이미지 다운로드
+
 from urllib.request import Request, urlopen
 import json
 import os
-
-def image_download(url, filepath):
-    request = Request(url)
-    response = urlopen(request)
-    image_data = response.read()
-    file = open(filepath, 'wb')
-    file.write(image_data)
-    file.close()
-    print(url + '로 부터 ' + filepath + '에 다운로드 완료')
-
-mask_url = 'https://github.com/prajnasb/observations/raw/master/mask_classifier/Data_Generator/images/blue-mask.png'
-image_download(mask_url, 'data/mask.png')
-exit()
 
 api_url = 'https://api.github.com/repos/prajnasb/observations/contents/experiements/data/without_mask?ref=master'
 
@@ -41,4 +30,3 @@ for i in range(len(contents)):
     image_file = open('data/without_mask/' + content['name'], 'wb')
     image_file.write(image_data)
     print('다운로드 완료(' + str(i + 1) + '/' + str(len(contents)) + '): ' + content['name'])
-    break
